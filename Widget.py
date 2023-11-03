df98534742db6d654ee58de20b3adbb9ec4ce43a
Kitchejd <142835429+Kitchejd@users.noreply.github.com>
2023-11-03 17:16:31 -0400

    Create Widget.py

---
+++ Widget.py
@@ -0,0 +1,8 @@
+[{"name":"widget1","color":"blue"},
+{"name":"widget2","color":"green"},
+{"name":"widget3","color":"black"},
+{"name":"widgetX","color":"blue"}]
+print("Widget1 is blue")
+print("Widget2 is green")
+print("Widget3 is black")
+print("WidgetX is blue")
